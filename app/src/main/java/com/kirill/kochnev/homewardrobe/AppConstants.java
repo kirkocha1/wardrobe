173dbeba1e8182959743e0b8f423be2a632e2afc
Kirill Kochnev <kirkocha@gmail.com>
2018-08-12 17:11:52 +0300

    update constants

--- app/src/main/java/com/kirill/kochnev/homewardrobe/AppConstants.java
+++ app/src/main/java/com/kirill/kochnev/homewardrobe/AppConstants.java
@@ -12,7 +12,7 @@ public class AppConstants {
     public static final int REQ_HEIGHT = 380;
     public static final int REQ_WIDTH = 285;
     public static final int DEFAULT_ID = -1;
-    public static final int LIMIT = 1;
+    public static final int LIMIT = 10;
     public static final int MAX_COLLAGE_COUNT = 4;
     public static final int MIN_COLLAGE_COUNT = 2;
     public static final int BYTES_MB_COEFF = 1000 * 1000;
